9809b56fd18599f0ae6140a8e26b318f00e5307f
Hassan Shereen <connectcap890@gmail.com>
2026-06-20 03:38:58 +0500

    Increased the Time for JWT

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -44,7 +44,7 @@ exports.login = async (req, res) =>{
         if(!isMatch){
             return res.status(400).json({message: 'Invalid credentials'});
         }   
-        const token = jwt.sign({userId: user._id, userType: user.userType}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        const token = jwt.sign({userId: user._id, userType: user.userType}, process.env.JWT_SECRET, {expiresIn: '12h'});
         res.status(200).json({message: 'Login successful', token, userType: user.userType});
 
     }catch(error){
